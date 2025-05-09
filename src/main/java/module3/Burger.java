411d1d1439bf482928eb68a65947bd80b0bd3787
infinity <igor74853@yandex.ru>
2025-05-09 22:45:03 +0500

    09.05.2025 22/44

---
+++ src/main/java/module3/Burger.java
@@ -0,0 +1,17 @@
+package main.java.module3;
+
+public class Burger {
+  String ingredient;
+
+  public void setIngredient(String ingredient) {
+    this.ingredient = ingredient;
+  }
+
+  public void showBurger() {
+    if (ingredient != null) {
+      System.out.println("Бургер с " + ingredient);
+    } else {
+      System.out.println("Бургер без добавок");
+    }
+  }
+}
---
+++ src/main/java/module3/task3.java
@@ -0,0 +1,12 @@
+package main.java.module3;
+
+public class task3 {
+  public static void main(String[] args) {
+    Burger burger1 = new Burger();
+    burger1.showBurger(); // Бургер без добавок
+
+    Burger burger2 = new Burger();
+    burger2.setIngredient("сыром");
+    burger2.showBurger(); // Бургер с сыром
+  }
+}
